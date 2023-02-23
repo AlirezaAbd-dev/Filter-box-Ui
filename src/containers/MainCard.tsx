41391453404f367842e67d1a2b917aa1e4c67ddc
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2023-02-23 20:56:26 +0330

    Delete src directory

--- src/containers/MainCard.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function MainCard({ children }: { children: ReactNode }) {
-  return (
-    <div className="relative flex flex-col bg-card max-w-md w-80 min-h-30 mt-32 p-10 rounded-3xl z-10 after:content-[''] after:absolute after:w-[80%] after:h-5 after:bg-blueAccent after:top-[-5.2%] after:left-[10%] after:z-0 after:rounded-t-3xl before:content-[''] before:absolute before:w-[80%] before:h-5 before:bg-blueAccent before:bottom-[-5.2%] before:left-[10%] before:z-0 before:rounded-b-3xl">
-      {children}
-    </div>
-  );
-}
--- src/layouts/MainLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function MainLayout({ children }: { children: ReactNode }) {
-  return (
-    <main className="font-lato h-screen w-screen flex items-start justify-center bg-background text-indigo-200 overflow-auto">
-      {children}
-    </main>
-  );
-}
